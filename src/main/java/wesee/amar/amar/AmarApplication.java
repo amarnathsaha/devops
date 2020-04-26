9c562b33462b5f826b5a2a855f1c2471c490d745
amarnath <amarnathsaha@yahoo.in>
2020-04-26 15:47:21 +0530

    all added

---
+++ src/main/java/wesee/amar/amar/AmarApplication.java
@@ -0,0 +1,13 @@
+package wesee.amar.amar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wesee/amar/amar/AmarApplicationTests.java
@@ -0,0 +1,13 @@
+package wesee.amar.amar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
