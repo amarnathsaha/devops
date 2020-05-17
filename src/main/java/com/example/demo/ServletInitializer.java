248d562f4849681dba913b9b69e1bfaa6e003960
AmaRNatH <amarnath.amar.saha@gmail.com>
2020-05-17 15:05:06 +0530

    added

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -1,4 +1,4 @@
-package wesee.amar.amar;
+package com.example.demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AmarApplication.class);
+		return application.sources(DemoApplication.class);
 	}
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package wesee.amar.amar;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AmarApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
